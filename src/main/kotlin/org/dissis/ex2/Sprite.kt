7340e9f1c4644b2bf3c9973c69e0a17c9fa1174b
Tomas Fernandez <tfernandez@mulesoft.com>
2020-08-31 12:33:52 -0300

    added first exercises

---
+++ src/main/kotlin/org/dissis/ex2/Sprite.kt
@@ -0,0 +1,28 @@
+package org.dissis.ex2
+
+interface Sprite {
+
+    fun draw(graphics: Graphics)
+}
+
+class CircleSprite : Sprite{
+    override fun draw(graphics: Graphics) {
+        TODO("Not yet implemented")
+    }
+}
+
+class RectangleSprite : Sprite {
+    override fun draw(graphics: Graphics) {
+        TODO("Not yet implemented")
+    }
+}
+
+class ZeldaSprite : Sprite {
+    override fun draw(graphics: Graphics) {
+        TODO("Not yet implemented")
+    }
+}
+
+class Graphics {
+    // TODO("Not yet implemented")
+}
